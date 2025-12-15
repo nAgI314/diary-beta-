16d8bdef33754ea25ae2c3ef6a85374f6691d50b
nAgI314 <kurosawasyunnagi@gmail.com>
2025-12-15 16:56:36 +0900

    空の日をクリックしてもblankに飛んじゃうのを修正

--- frontend/src/components/day/Day.tsx
+++ frontend/src/components/day/Day.tsx
@@ -2,23 +2,27 @@ import type { DiaryDay } from "../grouping";
 import styles from "./styles.module.css";
 
 interface DayProps {
-    day: number;
-    isToday: boolean;
-    diaryDayData?: DiaryDay;
-} 
-
-export const Day = (props:DayProps) => {
-
-    return (
-        <div 
-            className={`
-                ${props.diaryDayData ? styles.diary_day : styles.day}
-                ${props.isToday ? styles.today : ""}
-            `} 
-            onClick={()=>window.open(props.diaryDayData?.markdown?.html_url, '_blank') }
-        >
-            <div>{props.day}</div>
-        </div>
-    )
+  day: number;
+  isToday: boolean;
+  diaryDayData?: DiaryDay;
 }
 
+export const Day = (props: DayProps) => {
+  const hasDiary = Boolean(props.diaryDayData?.markdown?.html_url);
+  return (
+    <div
+      className={`
+                ${props.diaryDayData ? styles.diary_day : styles.day}
+                ${props.isToday ? styles.today : ""}
+            `}
+      // onClick={()=>window.open(props.diaryDayData?.markdown?.html_url, '_blank') }
+      onClick={
+        hasDiary
+          ? () => window.open(props.diaryDayData!.markdown!.html_url, "_blank")
+          : undefined
+      }
+    >
+      <div>{props.day}</div>
+    </div>
+  );
+};
